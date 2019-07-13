1298bd9518dbba395fe8b2dd873907b85c2816c0
Max <intelmax3@gmail.com>
2019-07-13 12:19:45 +0300

    Переехал на React, настроил окружение, реструктуризация

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, `public`),
+    compress: false,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ],
+  },
+  devtool: `source-map`
+};

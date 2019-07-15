86a6f932a67a4569eae2ab5c23a91a3afe1dea24
Maxim Khoptyar <maxim.hoptyar@smartbics.com>
2019-07-15 11:06:20 +0300

    Убрал минификатор изображений

--- webpack.config.js
+++ webpack.config.js
@@ -45,31 +45,7 @@ module.exports = {
         test:/\.(gif|png|jpe?g|svg)$/i,
         use: [
           "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                optimizationLevel: 7,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75
-              }
-            }
-          }
-        ],
+        ]
       },
     ],
   },
